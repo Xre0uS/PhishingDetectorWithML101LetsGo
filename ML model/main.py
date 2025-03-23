d16b14cb8c3cb4d1ccf59b6e43979bf993ab096b
Xre0uS <hello@xre0us.io>
2025-03-23 17:19:15 +0800

    Add files via upload

--- ML model/main.py
+++ ML model/main.py
@@ -4,39 +4,109 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from six.moves import urllib
+import seaborn as sns
+import time
+
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.snowball import SnowballStemmer
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from nltk.tokenize import RegexpTokenizer  
+from nltk.stem.snowball import SnowballStemmer
+from sklearn.feature_extraction.text import CountVectorizer 
+from sklearn.pipeline import make_pipeline
+
+import pickle# use to dump model 
 
 import tensorflow.compat.v2.feature_column as fc
 
 import tensorflow as tf
+print(tf.__version__)
 
 dftrain = pd.read_csv('E:/Codes/Python/ML Phising Detector/Datasets/phishing_site_urls.csv')
 dfeval = pd.read_csv('E:/Codes/Python/ML Phising Detector/Datasets/test.csv')
-y_train = dftrain.pop('Label')
-y_eval = dfeval.pop('Label')
 
-CATEGORICAL_COLUMNS = ['URL']
+label_counts = pd.DataFrame(dftrain.Label.value_counts())
+print(label_counts)
 
-feature_columns = []
-for feature_name in CATEGORICAL_COLUMNS:
-  vocabulary = dftrain[feature_name].unique()
-  feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(feature_name, vocabulary))
+sns.set_style('darkgrid')
+sns.barplot(label_counts.index,label_counts.Label)
 
-def make_input_fn(data_df, label_df, num_epochs=5, shuffle=True, batch_size=32):
-  def input_function():  # inner function, this will be returned
-    ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))  # create tf.data.Dataset object with data and its label
-    if shuffle:
-      ds = ds.shuffle(1000)  # randomize order of data
-    ds = ds.batch(batch_size).repeat(num_epochs)  # split dataset into batches of 32 and repeat process for number of epochs
-    return ds  # return a batch of the dataset
-  return input_function  # return a function object for use
+def treat_data(datain):
+    tokenizer = RegexpTokenizer(r'[A-Za-z]+')
+    tokenizer.tokenize(datain.URL[0])
+    print('Getting words tokenized ...')
+    t0= time.perf_counter()
+    datain['text_tokenized'] = datain.URL.map(lambda t: tokenizer.tokenize(t))
+    t1 = time.perf_counter() - t0
+    print('Time taken',t1 ,'sec')
 
-train_input_fn = make_input_fn(dftrain, y_train)  # here we will call the input_function that was returned to us to get a dataset object we can feed to the model
-eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
+    stemmer = SnowballStemmer("english")
+    print('Getting words stemmed ...')
+    t0= time.perf_counter()
+    datain['text_stemmed'] = datain['text_tokenized'].map(lambda l: [stemmer.stem(word) for word in l])
+    t1= time.perf_counter() - t0
+    print('Time taken',t1 ,'sec')
 
-linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
-# We create a linear estimtor by passing the feature columns we created earlier
+    print('Getting joiningwords ...')
+    t0= time.perf_counter()
+    datain['text_sent'] = datain['text_stemmed'].map(lambda l: ' '.join(l))
+    t1= time.perf_counter() - t0
+    print('Time taken',t1 ,'sec')
 
-linear_est.train(train_input_fn)  # train
-result = linear_est.evaluate(eval_input_fn)  # get model metrics/stats by testing on tetsing data
+    dataout = datain
+    return dataout
 
-print(result['accuracy'])  # the result variable is simply a dict of stats about our model
+dftrain = treat_data(dftrain)
+dftrain.shape
+dftrain.sample(10)
+
+dfeval = treat_data(dfeval)
+dfeval.shape
+dfeval.sample(10)
+
+cv = CountVectorizer()
+feature = cv.fit_transform(dftrain.text_sent)
+feature[:5].toarray()
+
+trainX, testX, trainY, testY = train_test_split(feature, dftrain.Label)
+
+lr = LogisticRegression(max_iter=5000)
+lr.fit(trainX,trainY)
+
+lr.score(testX,testY)
+
+Scores_ml = {}
+Scores_ml['Logistic Regression'] = np.round(lr.score(testX,testY),2)
+
+print('Training Accuracy :',lr.score(trainX,trainY))
+print('Testing Accuracy :',lr.score(testX,testY))
+con_mat = pd.DataFrame(confusion_matrix(lr.predict(testX), testY),
+            columns = ['Predicted:Bad', 'Predicted:Good'],
+            index = ['Actual:Bad', 'Actual:Good'])
+
+
+print('\nCLASSIFICATION REPORT\n')
+print(classification_report(lr.predict(testX), testY,
+                            target_names =['Bad','Good']))
+
+print('\nCONFUSION MATRIX')
+plt.figure(figsize= (6,4))
+sns.heatmap(con_mat, annot = True,fmt='d',cmap="YlGnBu")
+
+pipeline_ls = make_pipeline(CountVectorizer(tokenizer = RegexpTokenizer(r'[A-Za-z]+').tokenize,stop_words='english'), LogisticRegression(max_iter=5000))
+trainX, testX, trainY, testY = train_test_split(dftrain.URL, dftrain.Label)
+pipeline_ls.fit(trainX,trainY)
+
+pipeline_ls.score(testX,testY)
+pickle.dump(pipeline_ls,open('phishing_detector.pkl','wb'))
+
+loaded_model = pickle.load(open('E:/Codes/Python/ML Phising Detector/phishing_detector.pkl', 'rb'))
+
+test = ['https://github.com/taruntiwarihp/Projects_DS/blob/master/Phishing Site URLs Prediction/prediction_app.py']
+probability = loaded_model.predict_proba(test)
+result = loaded_model.predict(test)
+print(probability, result)
