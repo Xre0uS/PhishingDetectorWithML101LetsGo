aee8caadd418ce66466a0de341e58cc4912aa6f3
Xre0uS <hello@xre0us.io>
2025-03-23 17:19:13 +0800

    Add files via upload

--- extension/test.js
+++ extension/test.js
@@ -1,6 +1,20 @@
- debugger;
-document.getElementById("startscan").addEventListener('click',function()
+
+document.getElementById("startscan").addEventListener('click', async() =>
 {
-    document.getElementById('test').innerHTML = "hello";
-    
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: getEmail,
+      });    
 });
+
+function getEmail()
+{
+    var path = '//*[@id=":oz"]/div[1]/div[2]/div[1]/table/tbody/tr[1]/td[1]/table/tbody/tr/td/h3/span/span[2]';
+    //alert(document.evaluate( path, document, null, XPathResult.STRING_TYPE, null ));
+    alert(document.getElementsByClassName("go")[0].innerHTML);
+}
+
+
+
