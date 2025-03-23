a45b73f0c4605eb911daf59f8088102fda143383
Xre0uS <hello@xre0us.io>
2025-03-23 17:19:13 +0800

    Add files via upload

--- extension/test.js
+++ extension/test.js
@@ -11,8 +11,6 @@ document.getElementById("startscan").addEventListener('click', async() =>
 
 function getEmail()
 {
-    var path = '//*[@id=":oz"]/div[1]/div[2]/div[1]/table/tbody/tr[1]/td[1]/table/tbody/tr/td/h3/span/span[2]';
-    //alert(document.evaluate( path, document, null, XPathResult.STRING_TYPE, null ));
     alert(document.getElementsByClassName("go")[0].innerHTML);
 }
 
