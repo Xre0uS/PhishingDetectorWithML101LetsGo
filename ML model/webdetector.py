48521781ba913acb6cda1c2e33dbfdb2b6c0686f
Xre0uS <hello@xre0us.io>
2025-03-23 17:19:15 +0800

    Add files via upload

--- ML model/webdetector.py
+++ ML model/webdetector.py
@@ -1,10 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
+FastAPI
 import joblib
 import os
 import numpy as np
 import json
 import sys
+from pydantic import BaseModel
+from typing import List
+
+class Input(BaseModel):
+    url: List[str]
+
+result = []
 
 app = FastAPI()
 
@@ -14,28 +22,31 @@ phishing_detector_ls = joblib.load(phishing_detector)
 
 # ML Aspect
 
-@app.get('/')
-async def predict(input):
-    X_predict = []
-    X_predict.append(str(input))
-    y_predict = phishing_detector_ls.predict(X_predict)
-    y_probability = phishing_detector_ls.predict_proba(X_predict)
-    predict = ' '.join(map(str, y_predict))
-    if y_predict == [1]:
-        output = {
-            "URL" : input,
-            "result" : predict, 
-            "probability" : (y_probability[0,1] * 100),
+@app.post('/')
+async def predict(inputs: Input):
+    result.clear()
+    for url in inputs.url:
+        X_predict = []
+        X_predict.append(str(url))
+        y_predict = phishing_detector_ls.predict(X_predict)
+        y_probability = phishing_detector_ls.predict_proba(X_predict)
+        predict = ' '.join(map(str, y_predict))
+        if y_predict == [1]:
+            output = {
+                "URL" : url,
+                "result" : predict, 
+                "probability" : (y_probability[0,1] * 100),
+                }
+            result.append(output)
+            
+        else:
+            output = {
+                "URL" : url,
+                "result" : predict, 
+                "probability" : (y_probability[0,0] * 100)
             }
-        result = output
-    else:
-        output = {
-            "URL" : input,
-            "result" : predict, 
-            "probability" : (y_probability[0,0] * 100)
-        }
-        result = output
-    
+            result.append(output)
+        
     return (result)
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=2408)
